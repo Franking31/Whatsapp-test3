529e2d051c25da158656f6d22816163099d066f8
Pange Frank <frankpange157@gmail.com>
2024-12-18 22:43:38 +0100

    creation des page de declaration metro.config et telechargement des dependance svg

--- src/Components/page/LoadingPages/LoadingPages.tsx
+++ src/Components/page/LoadingPages/LoadingPages.tsx
@@ -1,11 +1,13 @@
 import { View, Text, StyleSheet } from 'react-native';
 import React from 'react';
 
+import BackgroundImg from "../../../../assets/svg/loading_background.svg"
+
 export default function LoadingPages() {
   return (
     <View style={styles.root}>
       <View>
-        
+      <BackgroundImg />
       </View>
     </View>
   );
