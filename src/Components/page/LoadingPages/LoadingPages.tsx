b6835da203758c6755116bf4c1e49f1b1d22d2f2
Pange Frank <frankpange157@gmail.com>
2024-12-19 00:02:12 +0100

    yo

--- src/Components/page/LoadingPages/LoadingPages.tsx
+++ src/Components/page/LoadingPages/LoadingPages.tsx
@@ -1,13 +1,20 @@
 import { View, Text, StyleSheet } from 'react-native';
 import React from 'react';
 
-import BackgroundImg from "../../../../assets/svg/loading_background.svg"
+import BackgroundImg from "../../../../assets/svg/loading_background.svg";
+import LOGO from "../../../../assets/svg/Franders.svg"
 
 export default function LoadingPages() {
   return (
     <View style={styles.root}>
-      <View>
+      <View style={styles.bgStyleView}>
       <BackgroundImg />
+      </View>
+      <View style={styles.contentViewStyle}>
+        <LOGO />
+        <Text>
+            Franksapp
+        </Text>
       </View>
     </View>
   );
@@ -16,5 +23,18 @@ export default function LoadingPages() {
 const styles = StyleSheet.create({
   root: {
     position: "relative",
+    width: "100%",
   },
+  bgStyleView: {
+    width: "100%",
+  },
+  contentViewStyle:{
+    position: "absolute",
+    display: "flex",
+    alignItems: "center",
+    justifyContent:"center",
+    width:"100%",
+    height:"100%",
+  },
+
 });
